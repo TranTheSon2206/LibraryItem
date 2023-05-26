f0eebe96d174ccd54a61edee686df7776c79e1fd
TranTheSon2206 <109707502+TranTheSon2206@users.noreply.github.com>
2023-05-26 21:38:16 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,14 +1,16 @@
-﻿using _26_5._1;
+using _26_5._1;
 
 class program
 {
     static void Main(string[] args)
     {
-        book book1 = new book("Life of Pi", "micahel", 1996,"Sience Figtion");
+        book book1 = new book("Life of Pi", "Micahel", 1996,"Sience Figtion");
         Console.WriteLine(book1.ToString());
+        Console.WriteLine("\n");
 
-        magazine magazine1 = new magazine("Sport","owen",2001,3600);
+        magazine magazine1 = new magazine("Sport","Owen",2001,3600);
         Console.WriteLine(magazine1.ToString());
+        Console.WriteLine("\n");
 
         dvd dvd1 = new dvd("Tom and Jerry", "Bathesa", 1993, "plastic");
         Console.WriteLine(dvd1.ToString());
